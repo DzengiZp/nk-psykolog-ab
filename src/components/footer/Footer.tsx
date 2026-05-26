4e568dd2abad8d38a844ca559f0f54b3a79e371e
Dzengiz Prentic <dzengiz.prentic@hotmail.com>
2026-05-26 16:46:35 +0200

    Removed redundant copies of next folder files.

--- src/components/footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./footer.module.css"
-
-export default function Footer() {
-  return (
-    <footer className={styles.footerColor}>
-      <p className={styles.copyrightNotice}>© 2024 Nathalie Korhonen Psykolog AB. All rights reserved.</p>
-    </footer>
-  );
-};
--- src/components/header/Header.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./header.module.css"
-
-export default function Header() {
-  return (
-    <header className={styles.headerColor}>
-      <h1 className={styles.headerTitle}>Header</h1>
-    </header>
-  );
-};
