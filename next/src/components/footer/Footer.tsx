cf92e6657627d69dc5f8a5f3dd337442f03a1137
Dzengiz Prentic <dzengiz.prentic@hotmail.com>
2026-05-27 16:29:21 +0200

    Updated footer component styling and formatted with prettier.

--- next/src/components/footer/Footer.tsx
+++ next/src/components/footer/Footer.tsx
@@ -2,7 +2,7 @@ import styles from "./footer.module.css"
 
 export default function Footer() {
   return (
-    <footer className={styles.footerColor}>
+    <footer className={styles.footerContainer}>
       <p className={styles.copyrightNotice}>© 2024 Nathalie Korhonen Psykolog AB. All rights reserved.</p>
     </footer>
   );
