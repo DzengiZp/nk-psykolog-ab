241d8a6ac11f6b37144e89feb4f2c6e28ea65766
Dzengiz Prentic <dzengiz.prentic@hotmail.com>
2026-05-27 16:29:07 +0200

    Updated header component styling and formatted with prettier.

--- next/src/components/header/Header.tsx
+++ next/src/components/header/Header.tsx
@@ -1,19 +1,26 @@
-import styles from "./header.module.css"
+import styles from './header.module.css';
+import Image from 'next/image';
 
 const buttons = [
-    { Title: "Startsida", Slug: "/" },
-    { Title: "Om Mig", Slug: "/about" },
-    { Title: "Psykoterapi", Slug: "/psykoterapi" },
-    { Title: "Kontakt", Slug: "/contact" }
-]
+  { Title: 'Startsida', Slug: '/' },
+  { Title: 'Om Mig', Slug: '/about' },
+  { Title: 'Psykoterapi', Slug: '/psykoterapi' },
+  { Title: 'Kontakt', Slug: '/contact' },
+];
 
 export default function Header() {
-    return (
-        <header className={styles.headerContainer}>
-            <h1 className={styles.headerTitle}>Nathalie Korhonen Psykolog AB</h1>
-            <div className={styles.headerButtonsContainer}>
-                {buttons.map(button => <button className={styles.headerButtons}>{button.Title}</button>)}
-            </div>
-        </header>
-    );
-};
+  return (
+    <header className={styles.headerContainer}>
+      {/* <Image src={"/logo.svg"} alt={""} width={569} height={40} /> */}
+      <div className={styles.headerButtonsContainer}>
+        {buttons.map(button => (
+          <button
+            key={button.Title}
+            className={styles.headerButtons}>
+            {button.Title}
+          </button>
+        ))}
+      </div>
+    </header>
+  );
+}
