8afec60c5c2a81e8911a0baf3478775acf4f8ccf
Dzengiz Prentic <dzengiz.prentic@hotmail.com>
2026-05-29 12:25:03 +0200

    Testing aboutme page.

--- next/src/app/aboutme/page.tsx
+++ next/src/app/aboutme/page.tsx
@@ -1,3 +1,19 @@
+import Image from 'next/image';
+import styles from './aboutme.module.css';
+import HeroImage from '../../../public/ship.jpg';
+
 export default function AboutMePage() {
-    return <h1>About me Page</h1>
+	return (
+		<>
+			<section className={styles.heroImageSectionContainer}>
+				<Image
+					className={styles.heroImage}
+					src={HeroImage}
+					alt='Hero Ship Image'
+					priority
+					fill
+				/>
+			</section>
+		</>
+	);
 }
