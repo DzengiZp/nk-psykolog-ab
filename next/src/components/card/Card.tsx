ddc29ef24c0999b1a6d3e45ccaf61c7235f8384c
Dzengiz Prentic <dzengiz.prentic@hotmail.com>
2026-05-29 12:27:27 +0200

    Added responsiveness for font size.
    Refactored Card component for duplicated code.

--- next/src/components/card/Card.tsx
+++ next/src/components/card/Card.tsx
@@ -2,58 +2,54 @@ import styles from './card.module.css';
 import Image from 'next/image';
 
 type CardProps = {
-  title?: string;
-  imageSrc?: string;
-  imageAlt?: string;
-  imageWidth?: number;
-  imageHeight?: number;
-  imageRounded?: boolean;
-  imageLoading?: 'lazy' | 'eager';
-  text?: string;
-  flip?: boolean;
+	flip?: boolean;
+	title?: string;
+	text?: string;
+	imageSrc?: string;
+	imageAlt?: string;
+	imageWidth?: number;
+	imageHeight?: number;
+	imageRounded?: boolean;
+	imageLoading?: 'lazy' | 'eager';
 };
 
-export default function Card({ title, imageSrc, imageAlt, text, imageWidth, imageHeight, imageRounded, imageLoading, flip }: CardProps) {
-  return (
-    <div className={styles.cardContainer}>
-      {flip ?
-        <>
-          <div className={styles.textContainer}>
-            {title && <h2 className={styles.cardTitle}>{title}</h2>}
-            {text && <p className={styles.aboutInformation}>{text}</p>}
-          </div>
-          <div className={styles.imageContainer}>
-            {imageSrc && (
-              <Image
-                className={imageRounded ? `${styles.imageRounded}` : `${styles.imageSquared}`}
-                src={imageSrc}
-                alt={imageAlt ?? ''}
-                width={imageWidth}
-                height={imageHeight}
-                loading={imageLoading}
-              />
-            )}
-          </div>
-        </>
-      : <>
-          <div className={styles.imageContainer}>
-            {imageSrc && (
-              <Image
-                className={imageRounded ? `${styles.imageRounded}` : `${styles.imageSquared}`}
-                src={imageSrc}
-                alt={imageAlt ?? ''}
-                width={imageWidth}
-                height={imageHeight}
-                loading={imageLoading}
-              />
-            )}
-          </div>
-          <div className={styles.textContainer}>
-            {title && <h2 className={styles.cardTitle}>{title}</h2>}
-            {text && <p className={styles.aboutInformation}>{text}</p>}
-          </div>
-        </>
-      }
-    </div>
-  );
+export default function Card({ flip, title, text, imageSrc, imageAlt, imageWidth, imageHeight, imageRounded, imageLoading }: CardProps) {
+	const imageBlock = imageSrc && (
+		<div className={styles.imageContainer}>
+			{imageSrc && (
+				<Image
+					className={imageRounded ? `${styles.imageRounded}` : `${styles.imageSquared}`}
+					src={imageSrc}
+					alt={imageAlt ?? 'A picture'}
+					width={imageWidth ?? 300}
+					height={imageHeight ?? 300}
+					style={{ width: '50%', height: 'auto' }}
+					loading={imageLoading}
+					sizes='(max-width: 768px) 80vw, 300px'
+				/>
+			)}
+		</div>
+	);
+
+	const textBlock = text && (
+		<div className={styles.textContainer}>
+			{title && <h2 className={styles.cardTitle}>{title}</h2>}
+			{text && <p className={styles.aboutInformation}>{text}</p>}
+		</div>
+	);
+
+	return (
+		<div className={styles.cardContainer}>
+			{flip ?
+				<>
+					{textBlock}
+					{imageBlock}
+				</>
+			:	<>
+					{imageBlock}
+					{textBlock}
+				</>
+			}
+		</div>
+	);
 }
